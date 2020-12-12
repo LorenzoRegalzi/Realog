5131f04ba7bd8d80a66128ba276950a5d86bd07f
LorenzoRegalzi <lorenzo.regalzi@gmail.com>
2020-12-12 14:46:28 +0100

    /* 12/12/20 nav-response*/

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Navigation from './components/navigation'
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 
 import PageRenderer from './page-renderer'
+import { responsiveArray } from 'antd/lib/_util/responsiveObserve';
 
 function App() {
+  const user = {
+    firstName: 'Marco',
+    lastName: 'Rossi',
+  }
   return (
     <Router>
       <div className="App">
-        <Navigation />
+        <Navigation user={user}/>
         <Switch>
           <Route path="/:page" component={PageRenderer} />
           <Route path="/" render={() => <Redirect to="/home" />} />
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
+import {Avatar} from 'antd'
 
 const navlinks = [
     {
@@ -23,16 +24,26 @@ const navlinks = [
 
 
 
-export default function Navigation () {
+export default function Navigation ({user}) {
+    const [menuActive, setMenuActive] = useState(false)
+
+    
     return (
     <nav className='site-navigation'>
-        <span>Realog</span>
-        <ul>
+        <span className='menu-title'>Realog</span>
+        <div className={`menu-content-container ${menuActive && 'active'}`}>
+            <ul>
            { navlinks.map((link, index) => (
                <li key={index}>
                    <Link to={link.path}>{link.title}</Link>
                </li>
            ))}
-        </ul>
+           </ul>
+           <span className='menu-avatar-container'>
+            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={38}/>
+            <span className='menu-avatar-name'>{`${user.firstName}${user.lastName}`}</span>
+           </span>  
+        </div>
+        <i  className='ionicons icon ion-ios-menu' onClick={() => setMenuActive(!menuActive)}/>
     </nav>)
 }
