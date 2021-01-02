7a57264dfec0207b8dc6c837fdda0aa2f9be3347
LorenzoRegalzi <lorenzo.regalzi@gmail.com>
2021-01-02 14:18:11 +0100

    Responsive  /* 02-01-21  */

---
+++ src/assets/moks/featured.js
@@ -0,0 +1,32 @@
+import moment from 'moment'
+
+export default [
+    {
+        title: 'can anyone code?',
+        date: moment().format('MMMM DD, YYYY'),
+        link: '#',
+        image: 'https://picsum.photos/200/300',
+        categories: ['Tech Culture'],
+    },
+    {
+        title: 'Using AWS for storing data?',
+        date: moment().format('MMMM DD, YYYY'),
+        link: '#',
+        image: 'https://picsum.photos/200/300',
+        categories: ['Cloud'],
+    },
+    {
+        title: 'Popular Programming Languages in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        link: '#',
+        image: 'https://picsum.photos/200/300',
+        categories: ['Tech Culture', 'Tech News'],
+    },
+    {
+        title: 'can anyone code?',
+        date: moment().format('MMMM DD, YYYY'),
+        link: '#',
+        image: 'https://picsum.photos/200/300',
+        categories: ['Cloud'],
+    }
+]
---
+++ src/assets/moks/trending.js
@@ -0,0 +1,39 @@
+import moment from 'moment'
+
+export default [
+    {
+        title: 'software Engineer Salary in 2028',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'https://picsum.photos/200/300',
+    },
+    {
+        title: 'Brain hacks',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['React', 'Javascript'],
+        link: '#',
+        image: 'https://picsum.photos/200/300',
+    },
+    {
+        title: 'React vs Vue',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'https://picsum.photos/200/300',
+    },
+    {
+        title: 'A day in the life of sotware engineer',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Brain Healt'],
+        link: '#',
+        image: 'https://picsum.photos/200/300',
+    },
+    {
+        title: 'GraphQL vs Rest',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['React', 'Vue'],
+        link: '#',
+        image: 'https://picsum.photos/200/300',
+    }
+]
